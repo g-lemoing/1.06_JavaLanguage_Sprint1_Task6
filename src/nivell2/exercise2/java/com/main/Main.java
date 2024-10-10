fe18c416d2948fb4c5b25262fd93721c65f61421
g-lemoing <le.moing.gwenael@gmail.com>
2024-10-10 18:25:09 +0200

    Commit Varags N2Ex2

--- src/nivell2/exercise2/java/com/main/Main.java
+++ src/nivell2/exercise2/java/com/main/Main.java
@@ -6,34 +6,24 @@ import nivell2.exercise2.java.com.modules.Persona;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Creem 3 instàncies de classe Persona, i les afegim a una primera llista.");
+        System.out.println("Creem 1 instància de classe Persona.");
         Persona person1 = new Persona("Joan", "Vila", 56);
-        Persona person2 = new Persona("Ana", "Guasch", 30);
-        Persona person3 = new Persona("Albert", "Duran", 43);
-        ArrayList<Persona> list1 = new ArrayList<>();
-        list1.add(person1);
-        list1.add(person2);
-        list1.add(person3);
 
         System.out.println("Creem una segona llista i l'omplim amb 3 objectes File.");
         File file1 = new File("Resultats.txt");
         File file2 = new File("Config.txt");
         File file3 = new File("Instruccions.txt");
-        ArrayList<File> files = new ArrayList<>();
-        files.add(file1);
-        files.add(file2);
-        files.add(file3);
-        System.out.println("Creem una tercera llista i l'omplim amb 3 cadenes de text.");
+        List<File> files = Arrays.asList(file1, file2, file3);
         ArrayList<String> cadenes = new ArrayList<>(Arrays.asList("animal", "ocell", "guineu"));
-        System.out.println("Invoquem al mètode genèric, passant per paràmetre aquestes 3 llistes.");
-        System.out.println("Comprovem que es mostren per pantalla correctament i seguint l'ordre dels arguments:");
-        GenericMethods.consolePrint(list1, files, cadenes);
-
-        System.out.println("Invoquem al mètode genèric, canviant l'ordre en què passem aquestes 3 llistes per paràmetre.");
-        System.out.println("Comprovem que es mostren per pantalla correctament amb l'ordre dels arguments modificat:");
-        GenericMethods.consolePrint(cadenes, files, list1);
+        System.out.println("Invoquem el mètode genèric amb arguments Varargs que mostrarem per pantalla.");
+        System.out.println("\nProvarem primer amb dos arguments: una llista de tipus String i una altra de tipus File:");
+        GenericMethods.consolePrint1(cadenes, files);
+        System.out.println("Invoquem el mateix mètode, passant-li ara 4 arguments: els anteriors, més un objecte Persona i un Integer");
+        GenericMethods.consolePrint1(cadenes, 78, files, person1);
+        System.out.println("Comprovem que es mostren per consola els arguments passats com a paràmetre, independentment de la seva quantitat.");
     }
 }
--- src/nivell2/exercise2/java/com/modules/GenericMethods.java
+++ src/nivell2/exercise2/java/com/modules/GenericMethods.java
@@ -1,13 +1,11 @@
 package nivell2.exercise2.java.com.modules;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GenericMethods {
 
-    public static <T, U, V> void consolePrint(ArrayList<T> arg0, ArrayList<U> arg1, ArrayList<V> arg2){
-        System.out.println("Primer argument --> " + arg0);
-        System.out.println("Segon argument --> " + arg1);
-        System.out.println("Tercer argument --> " + arg2);
+    @SafeVarargs
+    public static <T> void consolePrint1(T... arg){
+        System.out.println(Arrays.toString(arg));
     }
-
 }
